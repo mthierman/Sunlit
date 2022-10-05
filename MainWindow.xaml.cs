dfa3ae21138783bde70bd927cd4fb4ac699b3a14
mthierman <mthierman@gmail.com>
2022-10-04 18:49:41 -0600

    Add notes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -27,10 +27,5 @@ namespace WinUI_Todo
         {
             this.InitializeComponent();
         }
-
-        private void myButton_Click(object sender, RoutedEventArgs e)
-        {
-            myButton.Content = "Clicked";
-        }
     }
 }
