78ac359dc1b6138ceccbde041e07bccdd8256156
mthierman <mthierman@gmail.com>
2022-10-06 22:03:18 -0600

    Snapshot

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,24 +1,9 @@
-﻿using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Microsoft.UI.Xaml.Shapes;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System;
+using System.ComponentModel;
 using System.Runtime.InteropServices;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.ApplicationModel;
-using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml;
 using Microsoft.Win32;
-using Windows.UI.Xaml;
-using System.Threading;
+using WinRT.Interop;
 
 namespace WinUI_Todo
 {
@@ -26,22 +11,59 @@ namespace WinUI_Todo
     {
         public App()
         {
-            this.InitializeComponent();
+            InitializeComponent();
             Environment.SetEnvironmentVariable("WEBVIEW2_ADDITIONAL_BROWSER_ARGUMENTS", "--enable-features=OverlayScrollbar,msOverlayScrollbarWinStyle:scrollbar_mode/minimal_mode,msOverlayScrollbarWinStyleAnimation");
-            //Environment.SetEnvironmentVariable("WEBVIEW2_ADDITIONAL_BROWSER_ARGUMENTS", "--enable-features=OverlayScrollbar,msOverlayScrollbarWinStyle:scrollbar_mode/full_mode,msOverlayScrollbarWinStyleAnimation");
         }
 
-        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
         {
             m_window = new MainWindow();
             m_window.Activate();
-            //new Thread(RunMessagePump).Start();
+            Test.SetPreferredAppMode(PreferredAppMode.AllowDark);
         }
 
-        //void RunMessagePump()
+        //public static event Microsoft.Win32.UserPreferenceChangedEventHandler UserPreferenceChanged;
+        //public delegate void UserPreferenceChangedEventHandler(object sender, UserPreferenceChangedEventArgs e);
+
+        class Test
+        {
+            [DllImport("uxtheme.dll", EntryPoint = "#135", SetLastError = true, CharSet = CharSet.Unicode)]
+            public static extern int SetPreferredAppMode(PreferredAppMode preferredAppMode);
+
+        }
+
+        private enum PreferredAppMode
+        {
+            Default,
+            AllowDark,
+            ForceDark,
+            ForceLight,
+            Max
+        }
+
+        //class Test2
         //{
-        //    System.Diagnostics.Debug.WriteLine("Starting Message Pump!!!");
+        //    [DllImport("uxtheme.dll", EntryPoint = "#132", SetLastError = true, CharSet = CharSet.Unicode)]
+        //    public static extern bool ShouldAppsUseDarkMode(bool attr);
+        //};
+
+        //class Test3
+        //{
+        //    [DllImport("dwmapi.dll")]
+        //    private static extern int DwmSetWindowAttribute(IntPtr hwnd, DWMWINDOWATTRIBUTE dwAttr, ref int pvAttr, int cbAttr);
+        //};
+
+        //private enum DWMWINDOWATTRIBUTE
+        //{
+        //    DWMWA_USE_IMMERSIVE_DARK_MODE = 20
         //}
+
+        //private static void SetWindowImmersiveDarkMode(IntPtr hWnd, bool enabled)
+        //{
+        //    int isEnabled = enabled ? 1 : 0;
+        //    int result = DwmSetWindowAttribute(hWnd, DWMWINDOWATTRIBUTE.DWMWA_USE_IMMERSIVE_DARK_MODE, ref isEnabled, sizeof(int));
+        //    if (result != 0) throw new Win32Exception(result);
+        //};
 
         private Window m_window;
     }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,27 +1,11 @@
 ﻿using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Microsoft.UI.Xaml.Shapes;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Microsoft.Web.WebView2.Core;
-using WinRT;
 using Microsoft.UI;
 using Microsoft.UI.Windowing;
 using WinRT.Interop;
 using System.Runtime.InteropServices;
 using System.ComponentModel;
-using Windows.UI.Core;
-using Microsoft.Win32;
 
 namespace WinUI_Todo
 {
@@ -29,57 +13,11 @@ namespace WinUI_Todo
     {
         private AppWindow m_AppWindow;
 
-        public enum DWMWINDOWATTRIBUTE
+        private AppWindow GetAppWindowForCurrentWindow()
         {
-            DWMWA_USE_IMMERSIVE_DARK_MODE = 20
-        }
-
-        public class PInvokeTheme
-        {
-            [DllImport("dwmapi.dll")]
-            public static extern int DwmSetWindowAttribute(IntPtr hwnd, DWMWINDOWATTRIBUTE dwAttr, ref int pvAttr, int cbAttr);
-        }
-
-        public static void SetWindowImmersiveDarkMode(IntPtr hWnd, bool enabled)
-        {
-            int isEnabled = enabled ? 1 : 0;
-            int result = PInvokeTheme.DwmSetWindowAttribute(hWnd, DWMWINDOWATTRIBUTE.DWMWA_USE_IMMERSIVE_DARK_MODE, ref isEnabled, sizeof(int));
-            if (result != 0) throw new Win32Exception(result);
-        }
-
-        public MainWindow()
-        {
-            Title = "Test Title";
-
             IntPtr hWnd = WindowNative.GetWindowHandle(this);
-            if (App.Current.RequestedTheme == ApplicationTheme.Dark)
-            {
-                SetWindowImmersiveDarkMode(hWnd, true);
-            }
-
-            InitializeComponent();
-
-            m_AppWindow = GetAppWindowForCurrentWindow();
-
-            m_AppWindow.Resize(new Windows.Graphics.SizeInt32 { Width = 480, Height = 480 });
-
-            //SystemEvents.UserPreferenceChanged += new UserPreferenceChangedEventHandler(SystemEvents_UserPreferenceChanged);
-            //StartListening();
-
-            if (AppWindowTitleBar.IsCustomizationSupported())
-            {
-                ExtendsContentIntoTitleBar = true;
-                SetTitleBar(AppTitleBar);
-            }
-            else
-            {
-                AppTitleBar.Visibility = Visibility.Collapsed;
-                //titleBar.IconShowOptions = IconShowOptions.HideIconAndSystemMenu;
-            }
-
-            //SetTitleBarColors();
-
-            //MyWebView.NavigationStarting += EnsureHttps;
+            WindowId wndId = Win32Interop.GetWindowIdFromWindow(hWnd);
+            return AppWindow.GetFromWindowId(wndId);
         }
 
         private void SwitchPresenter_CompOverlay(object sender, RoutedEventArgs e)
@@ -99,103 +37,53 @@ namespace WinUI_Todo
             }
         }
 
-        private AppWindow GetAppWindowForCurrentWindow()
+        //public enum DWMWINDOWATTRIBUTE
+        //{
+        //    DWMWA_USE_IMMERSIVE_DARK_MODE = 20
+        //}
+
+        //public class PInvokeTheme
+        //{
+        //    [DllImport("dwmapi.dll")]
+        //    public static extern int DwmSetWindowAttribute(IntPtr hwnd, DWMWINDOWATTRIBUTE dwAttr, ref int pvAttr, int cbAttr);
+        //}
+
+        //public static void SetWindowImmersiveDarkMode(IntPtr hWnd, bool enabled)
+        //{
+        //    int isEnabled = enabled ? 1 : 0;
+        //    int result = PInvokeTheme.DwmSetWindowAttribute(hWnd, DWMWINDOWATTRIBUTE.DWMWA_USE_IMMERSIVE_DARK_MODE, ref isEnabled, sizeof(int));
+        //    if (result != 0) throw new Win32Exception(result);
+        //}
+
+        public MainWindow()
         {
-            IntPtr hWnd = WindowNative.GetWindowHandle(this);
-            WindowId wndId = Win32Interop.GetWindowIdFromWindow(hWnd);
-            return AppWindow.GetFromWindowId(wndId);
+            //IntPtr hWnd = WindowNative.GetWindowHandle(this);
+            //if (App.Current.RequestedTheme == ApplicationTheme.Dark)
+            //{
+            //    SetWindowImmersiveDarkMode(hWnd, true);
+            //}
+
+            Title = "Todo";
+            InitializeComponent();
+
+            //m_AppWindow = GetAppWindowForCurrentWindow();
+            //m_AppWindow.Resize(new Windows.Graphics.SizeInt32 { Width = 480, Height = 480 });
         }
 
-        //public delegate void UserPreferenceChangedEventHandler(object sender, UserPreferenceChangedEventArgs e);
 
-        //public event UserPreferenceChangedEventHandler UserPreferenceChanged;
-
-        //public bool eventHandlersCreated;
-
-        //public void StartListening()
+        //private void Test(object sender, RoutedEventArgs e)
         //{
-        //    SystemEvents.UserPreferenceChanged += new UserPreferenceChangedEventHandler(SystemEvents_UserPreferenceChanged);
-        //    System.Diagnostics.Debug.WriteLine("Started listening!!!");
-        //    eventHandlersCreated = true;
-        //}
-
-        //public void StopListening()
-        //{
-        //    SystemEvents.UserPreferenceChanged -= new UserPreferenceChangedEventHandler(SystemEvents_UserPreferenceChanged);
-        //    System.Diagnostics.Debug.WriteLine("Stopped listening!!!");
-        //    eventHandlersCreated = false;
-        //}
-
-        //private void StartListening()
-        //{
-        //    SystemEvents.UserPreferenceChanged += new UserPreferenceChangedEventHandler(SystemEvents_UserPreferenceChanged);
-        //    System.Diagnostics.Debug.WriteLine("Started listening!!!");
-        //}
-
-        //private void SystemEvents_UserPreferenceChanged(object sender, UserPreferenceChangedEventArgs e)
-        //{
-        //    System.Diagnostics.Debug.WriteLine("Theme changed!!!");
-        //}
-
-        //public void WindowClosed(object sender, WindowEventArgs e)
-        //{
-        //    SystemEvents.UserPreferenceChanged -= new UserPreferenceChangedEventHandler(SystemEvents_UserPreferenceChanged);
-        //    System.Diagnostics.Debug.WriteLine("Closed!!!");
-        //}
-
-        //public void WindowClosed(object sender, WindowEventArgs e)
-        //{
-        //    if (eventHandlersCreated)
+        //    IntPtr hWnd = WindowNative.GetWindowHandle(this);
+        //    m_AppWindow = GetAppWindowForCurrentWindow();
+        //    var titleBar = m_AppWindow.TitleBar;
+        //    ToggleButton toggleButton = sender as ToggleButton;
+        //    if (((ToggleButton)sender).IsChecked == false)
         //    {
-        //        SystemEvents.UserPreferenceChanged -= new UserPreferenceChangedEventHandler(SystemEvents_UserPreferenceChanged);
-        //        System.Diagnostics.Debug.WriteLine("Closed!!!");
-        //        //StopListening();
-        //    }
-        //}
-
-        //private bool SetTitleBarColors()
-        //{
-        //    // Check to see if customization is supported.
-        //    // Currently only supported on Windows 11.
-        //    if (AppWindowTitleBar.IsCustomizationSupported())
-        //    {
-        //        if (m_AppWindow is null)
-        //        {
-        //            m_AppWindow = GetAppWindowForCurrentWindow();
-        //        }
-        //        var titleBar = m_AppWindow.TitleBar;
-
-        //        // Set active window colors
-        //        titleBar.ForegroundColor = Colors.White;
-        //        titleBar.BackgroundColor = Colors.Green;
-        //        titleBar.ButtonForegroundColor = Colors.White;
-        //        titleBar.ButtonBackgroundColor = Colors.SeaGreen;
-        //        titleBar.ButtonHoverForegroundColor = Colors.Gainsboro;
-        //        titleBar.ButtonHoverBackgroundColor = Colors.DarkSeaGreen;
-        //        titleBar.ButtonPressedForegroundColor = Colors.Gray;
-        //        titleBar.ButtonPressedBackgroundColor = Colors.LightGreen;
-
-        //        // Set inactive window colors
-        //        titleBar.InactiveForegroundColor = Colors.Gainsboro;
-        //        titleBar.InactiveBackgroundColor = Colors.SeaGreen;
-        //        titleBar.ButtonInactiveForegroundColor = Colors.Gainsboro;
-        //        titleBar.ButtonInactiveBackgroundColor = Colors.SeaGreen;
-        //        return true;
-        //    }
-        //    return false;
-        //}
-
-        //private void EnsureHttps(WebView2 sender, CoreWebView2NavigationStartingEventArgs args)
-        //{
-        //    String uri = args.Uri;
-        //    if (!uri.StartsWith("https://"))
-        //    {
-        //        MyWebView.ExecuteScriptAsync($"alert('{uri} is not safe, try an https link')");
-        //        args.Cancel = true;
+        //        SetWindowImmersiveDarkMode(hWnd, true);
         //    }
         //    else
         //    {
-        //        addressBar.Text = uri;
+        //        SetWindowImmersiveDarkMode(hWnd, false);
         //    }
         //}
     }
